675b54b410e1cb5687f47d0bb9bb1b8fb355d106
WilliamPerezBeltran <williampbeltranprogramador@gmail.com>
2021-03-08 16:44:53 -0500

    Add java object

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Java tutorial
 
 ## Reference
+[java princeton](https://introcs.cs.princeton.edu/java/home/)
 [java tutorial](https://www.w3schools.com/java/default.asp)
---
+++ javaObject.java
@@ -0,0 +1,7 @@
+public class javaObject{
+	int x = 5;
+	public static void main(String[] args){
+		javaObject myObj = new javaObject();
+		System.out.println(myObj.x);
+	}
+}
