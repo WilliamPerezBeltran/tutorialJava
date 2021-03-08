e2830f20d12de08aea0963f6eb5671016797f950
WilliamPerezBeltran <williampbeltranprogramador@gmail.com>
2021-03-08 10:37:38 -0500

    Add repository java

---
+++ HelloWorld.java
@@ -0,0 +1,24 @@
+// cocmentari
+/*multi lines 
+de la z a la z
+*/
+
+public class HelloWorld {
+	public static void main(String[] args){
+		int miNumero = 234;
+		float mifloatNumero0 = 25.6f;
+		double mifloatNumero1 = 25.6;
+		String miString = "hola";
+		char miChar = 'd';
+		boolean miBolean = true;
+		System.out.println("william perez al ataque");
+
+
+		 System.out.println(miNumero); 
+		 System.out.println(mifloatNumero0); 
+		 System.out.println(mifloatNumero1); 
+		 System.out.println(miString); 
+		 System.out.println(miChar); 
+
+	}
+}
