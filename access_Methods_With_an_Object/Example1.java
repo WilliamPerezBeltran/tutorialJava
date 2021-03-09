c023a5b4af996fdc11f695d1204ac1cff1263ca7
WilliamPerezBeltran <williampbeltranprogramador@gmail.com>
2021-03-09 10:13:36 -0500

    Add access methods with an object

---
+++ access_Methods_With_an_Object/Example1.java
@@ -0,0 +1,14 @@
+public class Example1{
+
+	static void sutamerchan(){
+		System.out.println("The function is static, no need a object to be created");
+
+	}
+	public void fullThrottle(){
+		System.out.println("The car is going as fast as it can!");
+	}
+
+	public void speed(int maxSpeed){
+		System.out.println("Max speed is: "+maxSpeed);	
+	}
+}
---
+++ access_Methods_With_an_Object/Main.java
@@ -0,0 +1,10 @@
+// import Example1;
+class Main{
+	public static void main(String[] args){
+		// Example1.sutamerchan();
+		Example1 myCar	= new Example1();
+		myCar.fullThrottle();
+		myCar.sutamerchan();
+		myCar.speed(234);	
+	}
+}
