16459337bf71901e410f647806bc31b4bb1aba1d
WilliamPerezBeltran <williampbeltranprogramador@gmail.com>
2021-03-10 13:29:59 -0500

    Add java arralist

---
+++ javaArraList/ExampleArrayList.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class ExampleArrayList{
+	public static void main(String[] args){
+		ArrayList<String> cars = new ArrayList<String>();
+		cars.add("volvo");
+		cars.add("BMW");
+		cars.add("Mazda");
+		System.out.println(cars);
+	}
+}
